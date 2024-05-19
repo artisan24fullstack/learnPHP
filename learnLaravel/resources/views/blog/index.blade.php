d2830d6b4871cd1bc7baafaabb08ff05539c1c26
artisan24fullstack <denisartisanfullstack@gmail.com>
2024-05-19 10:45:22 +0200

    blade category and tag name

--- learnLaravel/resources/views/blog/index.blade.php
+++ learnLaravel/resources/views/blog/index.blade.php
@@ -11,6 +11,24 @@
     @foreach ($posts as $post)
         <article>
             <h2>{{ $post->title }}</h2>
+            <p class="small">
+                @if ($post->category)
+                    Catégorie : <strong>{{ $post->category?->name }}</strong>
+                    @if (!$post->tags->isEmpty())
+                    @endif
+                @else
+                    Aucune catégorie
+                @endif
+                <span> | </span>
+                @if (!$post->tags->isEmpty())
+                    Tags:
+                    @foreach ($post->tags as $tag)
+                        <span class="badge bg-secondary">{{ $tag->name }}</span>
+                    @endforeach
+                @else
+                    Aucun tag
+                @endif
+            </p>
             <p>{{ $post->content }}</p>
             <p>
                 <a class="btn btn-primary" href="{{ route('blog.show', ['slug' => $post->slug, 'post' => $post->id]) }}">
