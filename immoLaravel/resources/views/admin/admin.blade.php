a07e22226085d5ece341d37862d3ce9195ce9ff4
artisan24fullstack <denisartisanfullstack@gmail.com>
2024-05-21 10:25:40 +0200

    CRUD options

--- immoLaravel/resources/views/admin/admin.blade.php
+++ immoLaravel/resources/views/admin/admin.blade.php
@@ -24,6 +24,18 @@
                 {{ session('success') }}
             </div>
         @endif
+
+
+        @if ($errors->any())
+
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         @yield('content')
 
     </div>
