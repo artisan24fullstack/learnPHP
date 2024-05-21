b8dfbd5f32f9336f4d93b760ed58bfa50fabbcf0
artisan24fullstack <denisartisanfullstack@gmail.com>
2024-05-21 14:19:17 +0200

    search query blade

--- immoLaravel/app/Http/Controllers/PropertyController.php
+++ immoLaravel/app/Http/Controllers/PropertyController.php
@@ -2,15 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SearchPropertiesRequest;
 use App\Models\Property;
 use Illuminate\Http\Request;
 
 class PropertyController extends Controller
 {
-    public function index()
+    public function index(SearchPropertiesRequest $request)
     {
-        $properties = Property::paginate(16);
-        return view('property.index', ['properties' => $properties]);
+        $query = Property::query();
+
+        if ($request->validated('price')) {
+
+            $query = $query->where('price', '<=', $request->validated('price'));
+        }
+        if ($request->validated('surface')) {
+
+            $query = $query->where('surface', '<=', $request->validated('surface'));
+        }
+        if ($request->validated('rooms')) {
+
+            $query = $query->where('rooms', '<=', $request->validated('rooms'));
+        }
+        if ($request->validated('title')) {
+
+            $query = $query->where('title', 'like', "%{$request->validated('title')}%");
+        }
+
+        // error Illegal operator and value combination.
+
+        /*
+        if ($request->has('price')) {
+
+            $query = $query->where('price', '<=', $request->input('price'));
+        }
+        if ($request->has('surface')) {
+
+            $query = $query->where('surface', '<=', $request->input('surface'));
+        }
+        if ($request->has('rooms')) {
+
+            $query = $query->where('rooms', '<=', $request->input('rooms'));
+        }
+        if ($request->has('title')) {
+
+            $query = $query->where('title', 'like', "%{$request->input('title')}%");
+        }
+        */
+
+
+        // $properties = Property::paginate(16);
+
+        return view('property.index', [
+
+            //'properties' => $properties
+            'properties' => $query->paginate(16),
+            'input' => $request->validated()
+
+        ]);
     }
 
     public function show(string $slug, Property $property)
--- immoLaravel/resources/views/property/index.blade.php
+++ immoLaravel/resources/views/property/index.blade.php
@@ -9,16 +9,29 @@
     <div class="bg-light p-5 mb-5 text-center">
         <form action="" method="get" class="container d-flex gap-2">
 
-            <input type="number" placeholder="Budget max" class="form-control" name="price" value="">
+            <input type="number" placeholder="Surface mimimum" class="form-control" name="surface"
+                value="{{ $input['surface'] ?? '' }}">
+            <input type="number" placeholder="Nombre de pièces min" class="form-control" name="rooms"
+                value="{{ $input['rooms'] ?? '' }}">
+            <input type="number" placeholder="Budget max" class="form-control" name="price"
+                value="{{ $input['price'] ?? '' }}">
+            <input placeholder="Mot clef" class="form-control" name="title" value="{{ $input['title'] ?? '' }}">
+
+            <button class="btn btn-primary btn-sm flex-grow-0">Rechercher</button>
         </form>
     </div>
     <div class="container mt-5">
         <div class="row">
-            @foreach ($properties as $property)
+            @forelse ($properties as $property)
                 <div class='col-3 mb-4'>
                     @include('property.card')
                 </div>
-            @endforeach
+
+            @empty
+                <div class='col'>
+                    Aucun bien ne correspond à votre recherche
+                </div>
+            @endforelse
         </div>
         <div class="my-4">
             {{ $properties->links() }}
