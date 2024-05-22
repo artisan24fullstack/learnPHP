6953b1c19de8afb05d764ca1fc1837132e54e8ab
artisan24fullstack <denisartisanfullstack@gmail.com>
2024-05-22 07:42:39 +0200

    remove false

--- immoLaravel/app/Http/Controllers/AuthController.php
+++ immoLaravel/app/Http/Controllers/AuthController.php
@@ -12,13 +12,7 @@ class AuthController extends Controller
 {
     public function login()
     {
-        /*
-        User::create([
-            'name' => 'John',
-            'email' => 'john@doe.fr',
-            'password' => Hash::make('0000')
-        ]);
-        */
+
         return view('auth.login');
     }
 
